7af3fb7a2484bd908a0b0695c6eed1473aac4d75
Wilson Guimarães <wegneto@gmail.com>
2017-08-11 11:20:48 +0100

    Finishing example

--- lambda-expressions/lambda-expressions/Program.cs
+++ lambda-expressions/lambda-expressions/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 namespace lambdaexpressions
 {
 	public class Program
@@ -12,10 +15,22 @@ namespace lambdaexpressions
 			Console.WriteLine(divLambda(2, 3));
 			Console.WriteLine(divLambda(50, 10));
 
-			//Without lambda
+			//Not Using lambda
 			Division div = Divide;
 			Console.WriteLine(div(2, 3));
 			Console.WriteLine(div(50, 10));
+
+            IEnumerable<string> values = new List<string>() { "hello", "Test 1", "Test 2", "Test 3" };
+            //Using lambda
+            Console.WriteLine(values.Any(s => s.Contains("hell")));
+            //Not Using lambda
+            foreach (string s in values) 
+            {
+                if (s.Contains("hell")) 
+                {
+                    Console.WriteLine(true);
+                }    
+            }
 		}
 
 		public static int Divide(int x, int y)
