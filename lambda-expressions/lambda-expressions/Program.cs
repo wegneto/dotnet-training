d56082d7a9fb36aff7a8f074bbf2b2e1b75119c7
Wilson Guimarães <wegneto@gmail.com>
2017-08-11 11:07:44 +0100

    Creating first class

---
+++ lambda-expressions/lambda-expressions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace lambdaexpressions
+{
+	public class Program
+	{
+		delegate int Division(int number1, int number2);
+
+		static void Main(string[] args)
+		{
+			//Using lambda
+			Division divLambda = (x, y) => x / y;
+			Console.WriteLine(divLambda(2, 3));
+			Console.WriteLine(divLambda(50, 10));
+
+			//Without lambda
+			Division div = Divide;
+			Console.WriteLine(div(2, 3));
+			Console.WriteLine(div(50, 10));
+		}
+
+		public static int Divide(int x, int y)
+		{
+			return x / y;
+		}
+	}
+}
